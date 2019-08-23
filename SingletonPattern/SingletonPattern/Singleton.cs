fdddb7e09226bb5af43bc94922f34aea506d8808
Balazs Nemeth <Balazs_Nemeth@epam.com>
2019-08-23 11:34:13 +0200

    Singleton pattern

---
+++ SingletonPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        private int numberOfInstance = 0;
+
+        private Singleton()
+        {
+            Console.WriteLine("Instantiating inside the private constructor");
+            numberOfInstance++;
+            Console.WriteLine("Number or instances = {0}", numberOfInstance);
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Console.WriteLine("We already have an instance now. Use it");
+                return instance;
+            }
+        }
+    }
+}
